f78c618b2e8cc398bcce68bf7a4e85b23efdf069
Leandro Nunes <nunes.pessoal@gmail.com>
2020-09-27 13:44:14 -0300

    data.sjon creation and config

--- fase-03-1/instructors.js
+++ fase-03-1/instructors.js
@@ -1,4 +1,6 @@
-//create
+const fs = require('fs')
+const data = require("./data.json")
+
 exports.post = function(req, res){
     const keys = Object.keys(req.body)
     for(key of keys){
@@ -6,5 +8,27 @@ exports.post = function(req, res){
             return res.send('Please, fill all fields')
         }
     }
-    return res.send(req.body)
+
+    let {avatar_url, birth, name, services, gender} = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
+    })
+  
 }
