c4dc7d40567ef1e0cdc42a411b95df0a509da846
Leandro Nunes <nunes.pessoal@gmail.com>
2020-10-02 21:10:50 -0300

    Formatting dates

--- fase-03-1/instructors.js
+++ fase-03-1/instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require("./data.json")
-const {age} = require("./utils")
+const {age, date} = require("./utils")
 const Intl = require('intl')
 
 exports.show = function(req, res){
@@ -63,5 +63,10 @@ exports.edit = function(req, res){
 
     if (!foundInstructor) return res.send("Instructor not found")
 
-    return res.render('instructors/edit', {instructor: foundInstructor})
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', {instructor})
 }
