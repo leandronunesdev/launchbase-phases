d0bd6c63fb6e8efc06ef34c48d06fb997bdc2eec
Leandro Nunes <nunes.pessoal@gmail.com>
2020-10-17 20:00:48 -0300

    Table created

--- fase-03-1/instructors.js
+++ fase-03-1/instructors.js
@@ -3,6 +3,10 @@ const data = require("./data.json")
 const {age, date} = require("./utils")
 const Intl = require('intl')
 
+exports.index = function(req, res){
+    return res.render("instructors/index", {instructors: data.instructors})
+}
+
 exports.show = function(req, res){
     const {id} = req.params
 
