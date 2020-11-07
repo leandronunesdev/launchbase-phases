8af492871926c6a2dc3576bcc9d4f2fc0e91bfcd
Leandro Nunes <nunes.pessoal@gmail.com>
2020-11-07 17:04:23 -0300

    Gym Manager finished

--- fase-03-1/controllers/instructors.js
+++ fase-03-1/controllers/instructors.js
@@ -1,10 +1,51 @@
 const fs = require('fs')
-const data = require("./data.json")
-const {age, date} = require("./utils")
+const data = require("../data.json")
+const {age, date} = require("../utils")
 const Intl = require('intl')
 
 exports.index = function(req, res){
     return res.render("instructors/index", {instructors: data.instructors})
+}
+
+exports.create = function(req, res){
+    return res.render("instructors/create")
+}
+
+exports.post = function(req, res){
+    const keys = Object.keys(req.body)
+    for(key of keys){
+        if (req.body[key] == ""){
+            return res.send('Please, fill all fields')
+        }
+    }
+
+    let {avatar_url, birth, name, services, gender} = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+
+    let id = 1
+    const lastInstructor = data.instructors[data.instructors.length - 1]
+    if (lastInstructor){
+        id = lastInstructor.id + 1
+    }
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+  
 }
 
 exports.show = function(req, res){
@@ -26,38 +67,6 @@ exports.show = function(req, res){
     return res.render("instructors/show", {instructor})
 }
 
-exports.post = function(req, res){
-    const keys = Object.keys(req.body)
-    for(key of keys){
-        if (req.body[key] == ""){
-            return res.send('Please, fill all fields')
-        }
-    }
-
-    let {avatar_url, birth, name, services, gender} = req.body
-
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.instructors.length + 1)
-
-    data.instructors.push({
-        id,
-        avatar_url,
-        name,
-        birth,
-        gender,
-        services,
-        created_at
-    })
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-        if (err) return res.send("Write file error!")
-
-        return res.redirect("/instructors")
-    })
-  
-}
-
 exports.edit = function(req, res){
     const {id} = req.params
 
@@ -69,7 +78,7 @@ exports.edit = function(req, res){
 
     const instructor = {
         ...foundInstructor,
-        birth: date(foundInstructor.birth)
+        birth: date(foundInstructor.birth).iso
     }
 
     return res.render('instructors/edit', {instructor})
