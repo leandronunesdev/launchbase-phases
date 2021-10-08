7a9a0ce705c88eb7c842cbaf127c109b62f9dd9e
leandronunesdev <nunes.pessoal@gmail.com>
2021-10-08 19:34:23 -0300

    criando filtros da tabela

--- fase-03-1/public/scripts.js
+++ fase-03-1/public/scripts.js
@@ -1,8 +1,26 @@
-const currentPage = location.pathname
-const menuItems = document.querySelectorAll("header .links a")
+const currentPage = location.pathname;
+const menuItems = document.querySelectorAll('header .links a');
 
-for (item of menuItems){
-    if (currentPage.includes(item.getAttribute("href"))){
-        item.classList.add("active")
-    }
+for (item of menuItems) {
+  if (currentPage.includes(item.getAttribute('href'))) {
+    item.classList.add('active');
+  }
 }
+
+// Paginação
+
+let totalPages = 20,
+  selectedPage = 15,
+  pages = [];
+
+for (let currentPage = 0; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
+    pages.push(currentPage);
+  }
+}
+
+console.log(pages);
